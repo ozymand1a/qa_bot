27c50729c8a5766a13d2b663e34459a3a25ded3c
gerpetrum <gerpetrum@yandex.ru>
2023-04-14 23:46:56 +0800

    update entrypoint.sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ echo "Everything is fine with pytest :) \n"
 
 echo "Run mypy checking ..."
 mypy .
-echo "Everything is fine with pytest :) \n"
+echo "Everything is fine with mypy :) \n"
 
 echo "Run flake8 checking ..."
 flake8 .
-echo "Everything is fine with pytest :) \n"
+echo "Everything is fine with flake8 :) \n"
