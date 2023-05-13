8862c42e0b87ef4165a8f0717a1fb9fa2522de73
Kazybek Askarbek <k.askarbek@innopolis.university>
2023-05-13 20:51:22 +0000

    better printing

--- app/client.py
+++ app/client.py
@@ -1,13 +1,15 @@
 import requests
 from fire import Fire
+from pprint import pprint
+import json
 
 
-def run(port: int = 8000):
+def run(question: str = "What is the capital of France?", port: int = 8000):
     url = f"http://0.0.0.0:{port}/answer"
-    data = {"question": "What is the capital of France?"}
+    data = {"question": question}
 
     response = requests.get(url, json=data)
-    print(response.json())
+    print(response.json()["answer"])  # json.dumps(, indent=4))
 
 
 if __name__ == "__main__":
